2679b6d5e85465a94f91ceba71a1512c302659e6
kaylemarchipro <kaylem.thomas-booth@archipro.co.nz>
2024-03-20 14:18:50 +1300

    fqcwafcca

---
+++ migrations/v1.1.4__gege.sql
@@ -0,0 +1,8 @@
+USE SCHEMA DEMO; 
+create or replace TABLE HELLO_WORLD
+(
+   FIRST_NAME VARCHAR
+  ,LAST_NAME VARCHAR
+  ,sex varchar
+  ,Age number 
+);
---
+++ v1.1.5__fuck.sql
@@ -0,0 +1,9 @@
+
+USE SCHEMA DEMO; 
+create or replace TABLE FUCKKKKKKKK
+(
+   FIRST_NAME VARCHAR
+  ,LAST_NAME VARCHAR
+  ,sex varchar
+  ,Age number 
+);
